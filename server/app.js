3c37c9e8a87b47743ae874d8ee0424e62dcb6a34
jimmyfargo <zblizzard@live.com>
2017-09-24 23:44:51 -0500

    post_stuff db name....

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 import routes from './routes'
 import bodyParser from 'body-parser'
 
-mongoose.connect('mongodb://localhost:27017/test', () => {
+mongoose.connect('mongodb://localhost:27017/post_stuff', () => {
     console.log('connected to mongodb')
 })
 
